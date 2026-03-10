455050bf61e9a82b0ccab97adfb1c3f2eda704c7
omarfonsek123 <omarfonseca309555@correo.itm.edu.co>
2026-03-10 14:58:01 -0500

    add: Update function

---
+++ code/product/product.controller.js
@@ -0,0 +1,19 @@
+const service = require("./product.service");
+
+const updateProduct = (req, res, next) => {
+    const { id } = req.params;
+    const  data  = req.body;
+    service
+    .updateProduct(id, data)
+    .then((result) => {
+        if (result.success) {
+            res.status(result.status).json(result.updatedProduct);
+        }
+        else {
+            res.status(result.status).json({ error: result.error });
+        }
+    })
+    .catch(next);
+    }
+
+module.exports = { updateProduct };
