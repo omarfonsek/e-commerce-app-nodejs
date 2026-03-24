e8a95aba745a81ed349f5ec764a5e0149a9fb77c
omarfonsek <omarfonsecamontes@hotmail.com>
2026-03-24 15:29:22 -0500

    Revert "Revert "Feature/put product""

--- code/products/routes.js
+++ code/products/routes.js
@@ -2,12 +2,6 @@ const express = require("express");
 const router = express.Router();
 const products = require("./product.controller");
 
-router.get("/", products.getAllProducts);
-router.get("/:id", products.getProductById);
-router.post("/", products.createProduct);
+router.put("/:id", products.updateProduct);
 
 module.exports = router;
-
-
-
-
