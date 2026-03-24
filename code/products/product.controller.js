410bb571191f1c7d3a1941973ee3fa7505de4d66
omarfonsek <omarfonsecamontes@hotmail.com>
2026-03-24 15:05:17 -0500

    Revert "Feature/put product"

--- code/products/product.controller.js
+++ code/products/product.controller.js
@@ -1,20 +1,32 @@
 const service = require("./product.service");
 
-const updateProduct = (req, res, next) => {
-    const { id } = req.params;
-    const  data  = req.body;
+const getAllProducts = (req, res, next) => {
     service
-    .updateProduct(id, data)
-    .then((result) => {
-        if (result.success) {
-            res.status(result.status).json(result.updatedProduct);
-        }
-        else {
-            res.status(result.status).json({ error: result.error });
-        }
+    .getAllProducts()
+    .then((data) => {
+        res.status(data.status).json(data);
     })
     .catch(next);
-    }
+}
 
-module.exports = { updateProduct };
+const getProductById = (req, res, next) => {
+    const { id } = req.params;
+    service
+    .getProductById(id)
+    .then((data) => {
+        res.status(data.status).json(data);
+    })
+    .catch(next);
+}
 
+const createProduct = (req, res, next) => {
+    service
+    .createProduct(req.body)
+    .then((data) => {
+        res.status(data.status);
+        res.json(data);
+    })
+    .catch(next); // Se envía el error al middleware global de erroes
+}
+
+module.exports = { getAllProducts, getProductById, createProduct };
