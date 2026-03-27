c445d0604a59150d20408554908d09425953b244
omarfonsek123 <omarfonseca309555@correo.itm.edu.co>
2026-03-27 16:58:23 -0500

    delete:  products folder

--- code/products/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const products = require("./product.controller");
-
-router.put("/:id", products.updateProduct);
-
-module.exports = router;
